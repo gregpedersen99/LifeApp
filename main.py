355d8a94c35ab6eeb891f99c86fcb52e79aeb4c4
Greg Pedersen <47991971+gregpedersen99@users.noreply.github.com>
2024-09-12 12:10:28 -0700

    Update main.py
    Changing this to a web app interface, this is to test the hosting connection for heroku

--- main.py
+++ main.py
@@ -1,37 +1,13 @@
-#Imports for all files
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+  return "Welcome to the website of Greg pedersen"
+
+if __name__ == "__main__":
+  app.run()
 
 
-import homeCode as hc
-import tkinter as tk
-from datetime import date
-import function as fc
-#from tkinter import PhotoImage
-
-#from datetime import datetime 
-
-parent = tk.Tk()
-parent.geometry("300x200")
-parent.title("To Do List")
-myDate = date.today()
-todaysDate = tk.Label(parent, text = fc.convert_date(myDate), font = "50")
-todaysDate.pack()
-
-goals = [hc.Goal("Exercise"), hc.Goal("1 Hour of Reading"), hc.Goal("Journal"), hc.Goal("2 Hours of Personal Work"), hc.Goal("10 Minutes of Stretching")]
-
-
-for goal in goals:
-    hc.CheckButton(parent, goal)
-
-
-button = tk.Button(parent, text="Save", command=lambda: fc.get_outputs(goals))
-button.pack()
-
-parent.mainloop()
-
-
-
-#Now, it will add data into our database when we press "save"
-#Then, we need to get rid of the hardcode lock on the push to DB function since the intention is to not 
-#alwas have 5 goals here.
-#After this, we need to be able to adjust the start menu so it reflects the save state that we left it at 
-#When we last used it 
+#Set up my heroku account with this function that points to my web domain 
+#Set this up so that the output here points to the web domain that I currently own.
